e7491e48edf8e7dded3fbc9976cc4552b73fae9e
SprengerV <b.francis@sprengerv.com>
2021-03-26 21:14:08 -0400

    modified for remote db

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,12 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW, 
+    {
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
