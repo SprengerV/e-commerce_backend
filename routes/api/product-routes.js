9291fd76345a7447e6924ee20a8ae7d305ca5740
SprengerV <b.francis@sprengerv.com>
2021-03-29 12:07:37 -0400

    converted to use promises

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,30 +6,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  try {
-    const prods = await Product.findAll(
-      {
-        raw: true,
-        include: [Category, 'product_tags']
-      }
-    )
+  Product.findAll(
+    {
+      raw: true,
+      include: [Category, 'product_tags']
+    }
+  )
+  .then((prods) => {
     if (!prods) {
       res.status(404).json({ message: 'No products found' })
       return
     }
     res.status(200).json(prods)
-  } catch (err) {
+  })
+  .catch((err) => {
     res.status(500).json(err)
-  }
-});
+  });
+})
 
 // get one product
-router.get('/:id', async (req, res) => {
+router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  try {
-    const id = req.params.id
-    const prod = await Product.findOne(
+  const id = req.params.id
+  Product.findOne(
       {
         raw: true,
         where: {
@@ -37,31 +37,34 @@ router.get('/:id', async (req, res) => {
         },
         include: [Category, 'product_tags']
       }
-    )
+  )
+  .then((prod) => {
     if (!prod) {
       res.status(404).json({ message: `No products found with ID ${id}` })
       return
     }
     res.status(200).json(prod)
-  } catch (err) {
+  })
+  .catch((err) => {
     res.status(500).json(err)
-  }
+  })
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
+      categoryId: 2
       tagIds: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length > 0) {
+      if (typeof req.body.tagIds === 'array') {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -81,7 +84,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -122,24 +125,25 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  try { 
-    const prod = await Product.destroy(
-      {
-        where: {
-          id: req.params.id
-        }
+  Product.destroy(
+    {
+      where: {
+        id: req.params.id
       }
-    )
+    }
+  )
+  .then((prod) => {
     if (!prod) {
       res.status(404).json({ message: `Product ID ${req.params.id} not found` })
       return
     }
     res.status(200).json({ message: `Product ID ${req.params.id} deleted`})
-  } catch (err) {
+  })
+  .catch((err) => {
     res.status(500).json(err)
-  }
+  })
 });
 
 module.exports = router;
