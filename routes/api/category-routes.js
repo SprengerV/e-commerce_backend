cea033bf1f43e0a38d0f7b32f6be74a6a159167b
SprengerV <b.francis@sprengerv.com>
2021-03-29 12:17:47 -0400

    updated to use promises

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -84,7 +84,7 @@ router.put('/:id', (req, res) => {
   })
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   const id = req.params.id
   Category.destroy(
